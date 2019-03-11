964de2b8a29c216048184fbd67280f26876ca138
Konrad Kostępski <kkostepski@users.noreply.github.com>
2019-03-11 19:42:40 +0100

    Rewrite edit form components to react hooks

--- src/modules/bets/BetResultEditForm/BetResultEditForm.js
+++ src/modules/bets/BetResultEditForm/BetResultEditForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Flex } from 'reflexbox'
 import getFormData from 'get-form-data'
 import styled from 'styled-components'
@@ -6,96 +6,74 @@ import { Button, Input as InputComponent, InputNumber } from 'antd'
 
 import { Match } from '../../../components'
 
-class BetResultEditForm extends Component {
-  constructor(props) {
-    super(props)
+const BetResultEditForm = ({ bet, game, makeBet, loading }) => {
+  const [resultA, onResultAChange] = useState(bet.betNumbers.a)
+  const [resultB, onResultBChange] = useState(bet.betNumbers.b)
+  const groupPhase = game.phase === 'Grupa'
 
-    this.state = {
-      resultA: this.props.bet.betNumbers.a,
-      resultB: this.props.bet.betNumbers.b
-    }
-  }
+  return (
+    <form
+      autoComplete="off"
+      onSubmit={e => {
+        e.preventDefault()
 
-  onResultAChange(value) {
-    this.setState({ resultA: value })
-  }
+        const formData = getFormData(e.currentTarget)
+        const betNumbers = {
+          a: Number(formData.resultA),
+          b: Number(formData.resultB),
+          winInPenalties: formData.winInPenalties || null
+        }
+        const gameId = Number(game.id)
 
-  onResultBChange(value) {
-    this.setState({ resultB: value })
-  }
+        makeBet(gameId, bet.gambler.id, betNumbers)
+      }}
+    >
+      <Match
+        date={game.date}
+        phase={game.phase}
+        group={game.teamA.group}
+        teamACode={game.teamA.code}
+        teamAName={game.teamA.name}
+        teamBCode={game.teamB.code}
+        teamBName={game.teamB.name}
+        resultA={
+          <InputNumber
+            name="resultA"
+            defaultValue={bet.betNumbers.a}
+            onChange={value => onResultAChange(value)}
+          />
+        }
+        resultB={
+          <InputNumber
+            name="resultB"
+            defaultValue={bet.betNumbers.b}
+            onChange={value => onResultBChange(value)}
+          />
+        }
+      />
 
-  render() {
-    const { bet, game, makeBet, loading } = this.props
-    const { resultA, resultB } = this.state
-    const groupPhase = game.phase === 'Grupa'
+      {resultA === resultB && !groupPhase && (
+        <WinInPenaltiesContainer align="center">
+          <WinInPenaltiesText>Wygrany w rzutach karnych:</WinInPenaltiesText>
+          <Input
+            name="winInPenalties"
+            defaultValue={bet.betNumbers.winInPenalties}
+          />
+        </WinInPenaltiesContainer>
+      )}
 
-    return (
-      <form
-        autoComplete="off"
-        onSubmit={e => {
-          e.preventDefault()
-
-          const formData = getFormData(e.currentTarget)
-          const betNumbers = {
-            a: Number(formData.resultA),
-            b: Number(formData.resultB),
-            winInPenalties: formData.winInPenalties || null
-          }
-          const gameId = Number(game.id)
-
-          makeBet(gameId, bet.gambler.id, betNumbers)
-        }}
-      >
-        <Match
-          date={game.date}
-          phase={game.phase}
-          group={game.teamA.group}
-          teamACode={game.teamA.code}
-          teamAName={game.teamA.name}
-          teamBCode={game.teamB.code}
-          teamBName={game.teamB.name}
-          resultA={
-            <InputNumber
-              name="resultA"
-              defaultValue={bet.betNumbers.a}
-              onChange={value => this.onResultAChange(value)}
-            />
-          }
-          resultB={
-            <InputNumber
-              name="resultB"
-              defaultValue={bet.betNumbers.b}
-              onChange={value => this.onResultBChange(value)}
-            />
-          }
-        />
-
-        {resultA === resultB &&
-          !groupPhase && (
-            <WinInPenaltiesContainer align="center">
-              <WinInPenaltiesText>
-                Wygrany w rzutach karnych:
-              </WinInPenaltiesText>
-              <Input
-                name="winInPenalties"
-                defaultValue={bet.betNumbers.winInPenalties}
-              />
-            </WinInPenaltiesContainer>
-          )}
-
-        <SaveContainer justify="flex-end">
-          <Button
-            type="primary"
-            disabled={loading}
-            loading={loading}
-            htmlType="submit"
-          >
-            Zapisz
-          </Button>
-        </SaveContainer>
-      </form>
-    )
-  }
+      <SaveContainer justify="flex-end">
+        <Button
+          type="primary"
+          disabled={loading}
+          loading={loading}
+          htmlType="submit"
+        >
+          Zapisz
+        </Button>
+      </SaveContainer>
+    </form>
+  )
 }
 
 const Input = styled(InputComponent)`
--- src/modules/matches/MatchEditForm/MatchEditForm.js
+++ src/modules/matches/MatchEditForm/MatchEditForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Button } from 'antd'
 import { Flex } from 'reflexbox'
 import styled from 'styled-components'
@@ -7,107 +7,92 @@ import { InputNumber } from 'antd'
 
 import { Match } from '../../../components'
 
-class MatchEditForm extends Component {
-  constructor(props) {
-    super(props)
+const MatchEditForm = ({
+  data: { id, date, phase, group, teamA, teamB, result },
+  editMatch,
+  loading
+}) => {
+  const [resultA, onResultAChange] = useState(result.a)
+  const [resultB, onResultBChange] = useState(result.b)
+  const groupPhase = phase === 'Grupa'
 
-    this.state = {
-      resultA: this.props.data.result.a,
-      resultB: this.props.data.result.b
-    }
-  }
+  return (
+    <form
+      autoComplete="off"
+      onSubmit={e => {
+        e.preventDefault()
+        const formData = getFormData(e.currentTarget)
 
-  onResultAChange(inputValue) {
-    const value = inputValue ? Number(inputValue) : null
-
-    this.setState({ resultA: value })
-  }
-
-  onResultBChange(inputValue) {
-    const value = inputValue ? Number(inputValue) : null
-
-    this.setState({ resultB: value })
-  }
-
-  render() {
-    const { data, editMatch, loading } = this.props
-    const { resultA, resultB } = this.state
-    const { id, date, phase, group, teamA, teamB, result } = data
-    const groupPhase = phase === 'Grupa'
-
-    return (
-      <form
-        autoComplete="off"
-        onSubmit={e => {
-          e.preventDefault()
-
-          const formData = getFormData(e.currentTarget)
-
-          editMatch(id, {
-            a: Number(formData.resultA),
-            b: Number(formData.resultB),
-            aPenalties: formData.resultPenaltyA
-              ? Number(formData.resultPenaltyA)
-              : null,
-            bPenalties: formData.resultPenaltyB
-              ? Number(formData.resultPenaltyB)
-              : null
-          })
-        }}
-      >
-        <Match
-          date={date}
-          phase={phase}
-          group={group}
-          teamACode={teamA.code}
-          teamAName={teamA.name}
-          teamBCode={teamB.code}
-          teamBName={teamB.name}
-          resultA={
+        editMatch(id, {
+          a: Number(formData.resultA),
+          b: Number(formData.resultB),
+          aPenalties: formData.resultPenaltyA
+            ? Number(formData.resultPenaltyA)
+            : null,
+          bPenalties: formData.resultPenaltyB
+            ? Number(formData.resultPenaltyB)
+            : null
+        })
+      }}
+    >
+      <Match
+        date={date}
+        phase={phase}
+        group={group}
+        teamACode={teamA.code}
+        teamAName={teamA.name}
+        teamBCode={teamB.code}
+        teamBName={teamB.name}
+        resultA={
+          <InputNumber
+            name="resultA"
+            defaultValue={result.a}
+            onChange={inputValue => {
+              const value = inputValue ? Number(inputValue) : null
+              onResultAChange(value)
+            }}
+          />
+        }
+        resultB={
+          <InputNumber
+            name="resultB"
+            defaultValue={result.b}
+            onChange={inputValue => {
+              const value = inputValue ? Number(inputValue) : null
+              onResultBChange(value)
+            }}
+          />
+        }
+        resultPenaltyA={
+          shouldRenderPenaltyField({ groupPhase, resultA, resultB }) ? (
             <InputNumber
-              name="resultA"
-              defaultValue={result.a}
-              onChange={value => this.onResultAChange(value)}
+              name="resultPenaltyA"
+              defaultValue={result.aPenalties}
             />
-          }
-          resultB={
+          ) : null
+        }
+        resultPenaltyB={
+          shouldRenderPenaltyField({ groupPhase, resultA, resultB }) ? (
             <InputNumber
-              name="resultB"
-              defaultValue={result.b}
-              onChange={value => this.onResultBChange(value)}
+              name="resultPenaltyB"
+              defaultValue={result.bPenalties}
             />
-          }
-          resultPenaltyA={
-            shouldRenderPenaltyField({ groupPhase, resultA, resultB }) ? (
-              <InputNumber
-                name="resultPenaltyA"
-                defaultValue={result.aPenalties}
-              />
-            ) : null
-          }
-          resultPenaltyB={
-            shouldRenderPenaltyField({ groupPhase, resultA, resultB }) ? (
-              <InputNumber
-                name="resultPenaltyB"
-                defaultValue={result.bPenalties}
-              />
-            ) : null
-          }
-        />
+          ) : null
+        }
+      />
 
-        <SaveContainer justify="flex-end">
-          <Button
-            type="primary"
-            disabled={loading}
-            loading={loading}
-            htmlType="submit"
-          >
-            Zapisz
-          </Button>
-        </SaveContainer>
-      </form>
-    )
-  }
+      <SaveContainer justify="flex-end">
+        <Button
+          type="primary"
+          disabled={loading}
+          loading={loading}
+          htmlType="submit"
+        >
+          Zapisz
+        </Button>
+      </SaveContainer>
+    </form>
+  )
 }
 
 const shouldRenderPenaltyField = ({ groupPhase, resultA, resultB }) => {
